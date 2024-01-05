1bd574516da74b067040fbe45dc0f76bd1f67048
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-05 14:07:11 +0100

    button hinzugefügt

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,23 +10,30 @@ fetch("https://picsum.photos/v2/list")
             let imgUrl = bild.download_url
             let author = bild.author
             let id = bild.id
+            let url = bild.url
 
             console.log(imgUrl);
             console.log(author);
             console.log(id);
 
-            let finalBild = document.createElement("fig")
+            let finalBild = document.createElement("figure")
 
             const bildShowed = document.createElement("img")
             bildShowed.setAttribute("src", imgUrl)
             bildShowed.setAttribute("alt", author)
             finalBild.appendChild(bildShowed)
-
-            let showAuthor = document.createElement("figcaption")
+            const showAuthor = document.createElement("figcaption")
             showAuthor.textContent = author
             finalBild.appendChild(showAuthor)
+            const btn = document.createElement("button")
+            btn.textContent = "More infos"
+            btn.addEventListener("click", () => {
+                window.open(url, '_blank');
+            });
+            finalBild.appendChild(btn)
 
             document.querySelector("#photosFeed").appendChild(finalBild)
+
 
         });
 
